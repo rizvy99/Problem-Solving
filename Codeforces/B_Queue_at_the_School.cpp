b58bdfd837d7c7c66f5e7770703f3d1fafbcc814
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2024-02-05 00:12:03 +0600

    solved

---
+++ Codeforces/B_Queue_at_the_School.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define el "\n"
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define sortall(x) sort(all(x))
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t,n;
+    cin>>n>>t;
+    string s;
+    cin>>s;
+    while(t--){
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'B' && s[i+1] == 'G')
+            {
+                s[i] = 'G';
+                s[i+1] = 'B';
+                i++;
+            }
+        }
+    }
+    cout << s << el;
+    return 0;
+}
