6376782eefbbd8e9d0d3ba76e6915bb490233511
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2024-02-06 23:36:07 +0600

    Create C_K_th_Not_Divisible_by_n.cpp

---
+++ Codeforces/C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define el "\n"
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define sortall(x) sort(all(x))
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        ll n,k;
+        cin>>n>>k;
+        ll num=k/(n-1);
+        if((num+k)%n==0){
+            cout<<num+k-1<<el;
+        }else{
+            cout<<num+k<<el;
+        }
+     
+    }
+    return 0;
+}
