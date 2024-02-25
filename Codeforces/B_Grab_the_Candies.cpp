2d683ffd1c9977f3f2e301556d8caa9020ca3eda
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2024-02-25 21:31:08 +0600

    Create B_Grab_the_Candies.cpp

---
+++ Codeforces/B_Grab_the_Candies.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define el "\n"
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define sortall(x) sort(all(x))
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ar[n],sum=0,sum1=0;
+        for(int i=0;i<n;i++){
+            cin>>ar[i];
+            if(ar[i]%2==0){
+                sum+=ar[i];
+            }else{
+                sum1+=ar[i];
+            }
+        }
+        if(sum>sum1){cout<<"YES"<<el;}
+        else{cout<<"NO"<<el;}
+     
+    }
+    return 0;
+}
